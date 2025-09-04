cf0d9ce7b16ea0fd124b871f3c09cd06bc3a02f1
ShreyashGajbhiye453 <2023bit042@sggs.ac.in>
2025-09-04 11:26:02 +0530

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer-text">© 2025 Smart Student Election. All rights reserved.</p>
-    </footer>
-  );
-}
